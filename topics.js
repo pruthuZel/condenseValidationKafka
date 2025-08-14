643c4b28871b60074db366d5b4139d80fcb2dced
pruthuZel <pruthuZel@example.com>
2025-08-14 13:01:23 +0000

    DEV: Added error fixes

--- topics.js
+++ topics.js
@@ -13,7 +13,6 @@ async function run(){
                 password: '3uuqhR2WHaxzC',
               },
          })
-dsafasdfasdf
         const admin = kafka.admin();
         console.log("Connecting.....")
         console.log("Validation")
